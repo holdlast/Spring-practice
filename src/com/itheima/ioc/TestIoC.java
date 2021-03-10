4170421dde6151cd2de1d91c7567888401ac33e3
王朔 <44704865+holdlast@users.noreply.github.com>
2021-03-10 12:20:49 +0800

    Add files via upload

---
+++ src/com/itheima/ioc/TestIoC.java
@@ -0,0 +1,12 @@
+package com.itheima.ioc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestIoC {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserDao userDao = (UserDao) applicationContext.getBean("userDao");
+        userDao.say();
+    }
+}
